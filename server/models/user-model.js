cd1bb7a452e8f7c190ebb7f637506062bca00631
Fazo01 <fg7829098@gmail.com>
2026-01-05 17:19:04 +0500

    complete adding jwt code but it's still facing issue

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,6 +1,6 @@
-
 const mongoose=require('mongoose');
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -26,7 +26,7 @@ const userSchema=new mongoose.Schema({
 userSchema.pre('save',async function(next){
     const user=this
     if(!user.isModified('password')){
-        next()
+        return next()
     }
     try {
         const salt=await bcrypt.genSalt(10)
@@ -37,7 +37,19 @@ userSchema.pre('save',async function(next){
         next(error);
     }
 })
-
+//jsonwebtoken
+userSchema.methods.generateToken=async function(){
+    try {
+        return jwt.sign({
+            userId:this._id.toString(),
+            email:this.email,
+            isAdmin:this.isAdmin
+        },process.env.JWT_SECRET_KEY,{expiresIn:'30d'})
+        
+    } catch (error) {
+        console.error('Error generating token:', error);
+    }
+}
 
 const User=new mongoose.model('User',userSchema);
 module.exports=User;
