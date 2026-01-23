e5b9801b3465c1ff65e383e3bae5c229d3893141
MuhammadFaizan144 <fg7829098@gmail.com>
2026-01-23 07:26:28 +0500

    add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Service from './pages/Service.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 const App = () => {
   return (
     <>
-      our App component
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/service' element={<Service/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
