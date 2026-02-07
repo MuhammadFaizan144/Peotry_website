fb0fe965a10a997743da4b63969795c990fff71c
MuhammadFaizan144 <fg7829098@gmail.com>
2026-02-07 13:38:53 +0500

    41 jwt for admin panel

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const authController=require('../controllers/auth-controllers')
 const {getAllUsers,getAllContacts}=require('../controllers/admin-controllers')
-router.route('/user').get(getAllUsers)
-router.route('/contact').get(getAllContacts)
+router.route('/user').get(authController,getAllUsers)
+router.route('/contact').get(authController,getAllContacts)
 module.exports=router
