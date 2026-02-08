912d400be3913d016a4720e06e6cdff781bc3347
MuhammadFaizan144 <fg7829098@gmail.com>
2026-02-08 11:11:47 +0500

    42 Admin, authentication: if user isadmin or not

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -2,7 +2,8 @@ const express=require('express');
 const router=express.Router();
 const authMiddleware=require("../middleware/auth-middleware")
 const {getAllUsers,getAllContacts,getAllService}=require("../controllers/admin-controllers")
-router.route('/user').get(authMiddleware,getAllUsers)
+const adminMiddleware=require('../middleware/admin-middleware')
+router.route('/user').get(authMiddleware,adminMiddleware,getAllUsers)
 router.route('/contact').get(authMiddleware,getAllContacts)
 router.route('/service').get(authMiddleware,getAllService)
 module.exports=router
